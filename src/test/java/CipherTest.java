319a1411b1fc0f565adc5f8fc112968b9d58a7ce
Gaks98 <yvonnegax98@gmail.com>
2019-08-30 18:41:13 +0300

    Cipher instantiates correctly

--- src/test/java/CipherTest.java
+++ src/test/java/CipherTest.java
@@ -1,5 +1,17 @@
+import org.junit.*;
+
 import static org.junit.Assert.*;
 
 public class CipherTest {
+    @Test
+    public void Cipher_instantiatesCorrectly_true() {
+        Cipher testCipher = new Cipher("hello",3);
+        assertEquals(true,testCipher instanceof Cipher);
+    }
 
+//    @Test
+//    public void CipherInstantiatesWithPlainTextAndShift_true() {
+//        Cipher testCipher = new Cipher("hello",3);
+//        assertEquals();
+//    }
 }
