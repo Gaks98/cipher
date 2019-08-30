d4fffa63e93205030e86634bf12afd024b8622d8
Gaks98 <yvonnegax98@gmail.com>
2019-08-30 17:06:18 +0300

    calling and displaying encrypted message

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,18 +3,17 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Cipher cipher = new Cipher();
 
         System.out.println("Welcome to caesar-cipher, this is an encryption software \n" +
-                "enter a word and the number of shift you would like to shift each letter to:\n" +
+                "enter a word and the number of shift you would like to switch each letter to:\n" +
                 "Enter word ");
         String word = scanner.nextLine();
-
         System.out.println("Enter shift :");
         int shift = scanner.nextInt();
-//
-//        System.out.println("Encryption version :" + cipher.encrypt(plainText));
+        Cipher cipher = new Cipher(word,shift);
+
+        System.out.println("Encryption version :" + cipher.encrypt(cipher));
 //        System.out.println("Decrypted version : " + cipher.decrypt(cipher.encrypt(plainText)));
-//    }
+    }
 }
 
