0ad484cba1a11a1f7c55aa4f17388850bdaaba6b
Gaks98 <yvonnegax98@gmail.com>
2019-08-30 16:30:08 +0300

    initial project structure

---
+++ src/main/java/App.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        Cipher cipher = new Cipher();
+
+
+
+
+//        System.out.println("Welcome to caesar-cipher, this is an encryption software \n" +
+//                "enter a word and the number of shift you would like to shift each letter to:\n" +
+//                "Enter word ");
+//        String plainText = scanner.nextLine();
+//
+//        System.out.println("Enter shift :");
+//        int shift = scanner.nextInt();
+//
+//        System.out.println("Encryption version :" + cipher.encrypt(plainText));
+//        System.out.println("Decrypted version : " + cipher.decrypt(cipher.encrypt(plainText)));
+//    }
+}
+
