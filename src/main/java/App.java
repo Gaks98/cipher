b97e841d4d3beaacffe01f03b069884982e1bc5f
Gaks98 <yvonnegax98@gmail.com>
2019-08-30 16:59:55 +0300

    created scanner class and instance of cipher

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        Cipher cipher = new Cipher();
+        Scanner scanner = new Scanner(System.in);
+        Cipher cipher = new Cipher();
 
 
 
